135909dbe10d94f6ce3a9aa1f8c96efa70e8492b
JoseMRuiz <josemruiz6@gmail.com>
2025-05-20 17:25:57 -0300

    Ejercicio4 Resuelto

---
+++ Ejercicios/Ejercicio4/Ejercicio4.js
@@ -0,0 +1,10 @@
+document.getElementById('calcButton').addEventListener('click', function() {
+   
+    let peso = parseFloat(document.getElementById('peso').value);
+    let altura = parseFloat(document.getElementById('altura').value);
+
+   
+    let resultado = peso / (altura ** 2);;
+    
+    alert(`Su imc es de ${resultado}`);
+});
